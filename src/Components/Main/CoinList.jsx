c6d726de179833f9f878b63a282ae77011a5994b
Uttkarsh <uttkarshpatel9@gmail.com>
2022-02-11 05:43:28 +0530

    added add btn

--- src/Components/Main/CoinList.jsx
+++ src/Components/Main/CoinList.jsx
@@ -3,13 +3,13 @@ import React, { useEffect } from "react";
 import Loading from "./Loading";
 import {useUserCredentials} from "../../Modules/Context/UserContext";
 
-function CoinList({isLoading, coinInfo}){
+function CoinList({isLoading, coinInfo, onAddCoinHandler}){
     const [userCredentials, setUserCredentials] = useUserCredentials();
 
-	return <div className="coin-list">
+	return <div className="coin-list-container">
 		{
 			// isLoading ? <Loading /> :
-			<div>
+			<div className="coin-list">
 				{
                     coinInfo.map(coin => {
                         return <CoinCard coin={coin} key={coin.name} />
@@ -17,14 +17,24 @@ function CoinList({isLoading, coinInfo}){
                 }
 			</div>
 		}
+        <div className="add-coin-btn" onClick={onAddCoinHandler}>
+            +
+        </div>
 	</div>
 }
 
 function CoinCard({coin}){
     const name = coin.name;
     const currentPrice = coin.market_data.current_price.inr;
+    const image = coin.image;
+
     return <div className="coin-card">
-        {name}-{currentPrice}
+        <div className="title">
+            <img src={image.small} alt={`${name} logo`} className="icon" />
+            <span className="name">{name}</span>
+            <span className="currency">/INR</span>
+        </div>
+        <span className="price">₹ {currentPrice}</span>
     </div>
 }
 
