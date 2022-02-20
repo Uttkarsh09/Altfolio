27f29fb37231a785ab8f02c473bee1310ee9c075
Uttkarsh <uttkarshpatel9@gmail.com>
2022-02-20 23:23:58 +0530

    switched from credentials file to environment variables

--- src/Modules/Firebase/GetFirebaseInfo.js
+++ src/Modules/Firebase/GetFirebaseInfo.js
@@ -1,24 +1,10 @@
-import firebaseConfig from "./fbConfig";
+import {getFirebaseConfig} from "./fbConfig";
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
-// function getFirebaseConfig(){
-//     const firebaseConfig = {
-//         apiKey: process.env.APIKEY,
-//         authDomain: process.env.AUTHDOMAIN,
-//         projectId: process.env.PROJECTID,
-//         storageBucket: process.env.STORAGEBUCKET,
-//         messagingSenderId: process.env.MESSAGINGSENDERID,
-//         appId: process.env.APPID,
-//         measurementId: process.env.MEASUREMENTID
-//     };
-//     console.log("THIS IS THE FIREBASE CONFIG");
-//     console.log(firebaseConfig);
-//     return firebaseConfig;
-// }
 
-const firebaseApp = initializeApp(firebaseConfig);
+const firebaseApp = initializeApp(getFirebaseConfig());
 const db = getFirestore();
 const auth = getAuth();
 
